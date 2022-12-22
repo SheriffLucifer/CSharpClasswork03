d893907d1a62b4ed28a37ff306afed09b019af05
Oleg <olegashepelev@gmail.com>
2022-12-23 00:13:53 +0600

    Complete quarter coordinates

---
+++ 03/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите четверть системы коорлдинат от 1 до 4: ");
+int quarter = int.Parse(Console.ReadLine() ?? "");
+
+if (quarter == 1)
+{
+    Console.WriteLine("Координаты X > 0 и Y > 0");
+}
+else if (quarter == 2)
+{
+    Console.WriteLine("Координаты X < 0 и Y > 0");
+}
+else if (quarter == 3)
+{
+    Console.WriteLine("Координаты X < 0 и Y < 0");
+}
+else if (quarter == 4)
+{
+    Console.WriteLine("Координаты X > 0 и Y < 0");
+}
+else
+{
+    Console.WriteLine("Вы ошиблись! Такой четверти нет");
+}
