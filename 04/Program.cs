e8276a75a8fc02dcf684f41c6a2d480c90bd7f56
Oleg <olegashepelev@gmail.com>
2022-12-23 00:34:14 +0600

    Complete find distance

---
+++ 04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите координаты X первой точки: ");
+int XA = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите координаты Y первой точки: ");
+int YA = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите координаты X второй точки: ");
+int XB = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите координаты Y второй точки: ");
+int YB = int.Parse(Console.ReadLine() ?? "");
+
+double result = Math.Sqrt(Math.Pow(XA - XB, 2) + Math.Pow(YB - YA, 2));
+
+Console.WriteLine($"A ({XA}, {YA}); B ({XB}, {YB}) -> {result}");
